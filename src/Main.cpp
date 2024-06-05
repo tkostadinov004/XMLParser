764c773468f61f3776084b16386c62405b7deb65
tkostadinov004 <tkostadinov04@gmail.com>
2024-06-05 17:04:44 +0300

    Removed unnecessary code from main

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,13 +2,6 @@
 
 int main()
 {
-	MyVector<int> evens;
-	for (size_t i = 0; i <= 10; i += 2)
-	{
-		evens.push_back(i);
-	}
-	std::cout << evens.count([](const int& a) {return a % 2 == 0;});
-
 	Engine& engine = Engine::getInstance();
 	engine.run();
 }
